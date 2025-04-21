4b5ac470aa8ee85520f65e1025572338cc388033
Tapeline <yt.redstone.mail@gmail.com>
2025-04-21 21:19:54 +0500

    Fix CI

--- tests/package/assets/download_execs.ps1
+++ tests/package/assets/download_execs.ps1
@@ -5,4 +5,4 @@ iwr https://download.oracle.com/java/23/archive/jdk-23.0.2_windows-x64_bin.exe -
 iwr https://www.niisi.ru/kumir/kumir2-2.1.0-rc11-install.exe
 gdown https://drive.google.com/file/d/1_FWPloZue2wfwKchzJd_rWwyLl6Xv0Kf/view?usp=drive_link --fuzzy
 iwr https://pascalabc.net/downloads/PascalABCNETSetup.exe
-iwr https://www.python.org/ftp/python/3.13.1/python-3.13.1-amd64.exe
+#iwr https://www.python.org/ftp/python/3.13.1/python-3.13.1-amd64.exe
