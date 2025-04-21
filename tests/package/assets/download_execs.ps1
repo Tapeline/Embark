ea6b4ed87ccdfc7657c8bc8224c2367065f114dd
Tapeline <yt.redstone.mail@gmail.com>
2025-04-21 21:07:07 +0500

    Fix CI

--- tests/package/assets/download_execs.ps1
+++ tests/package/assets/download_execs.ps1
@@ -1,4 +1,3 @@
-cd assets
 pip install gdown
 iwr https://download-cdn.jetbrains.com/idea/ideaIC-2024.2.3.exe
 iwr https://download.jetbrains.com/python/pycharm-community-2024.2.3.exe
@@ -7,4 +6,3 @@ iwr https://www.niisi.ru/kumir/kumir2-2.1.0-rc11-install.exe
 gdown https://drive.google.com/file/d/1_FWPloZue2wfwKchzJd_rWwyLl6Xv0Kf/view?usp=drive_link --fuzzy
 iwr https://pascalabc.net/downloads/PascalABCNETSetup.exe
 iwr https://www.python.org/ftp/python/3.13.1/python-3.13.1-amd64.exe
-cd ..
